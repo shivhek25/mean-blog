6d2b93a14cee7b123e41eb53dfc766129d7ad60d
Shivam Singhal <shivamsinghal0610@gmail.com>
2017-10-30 02:02:28 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.post("/api/blogpost", createPost);
+
+function createPost(req, res){
+	var post = req.body;
+	console.log(post);
+}
+
+app.listen(3000);
